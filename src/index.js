aaa3623ead9651baa1ff6e9e5e8f25000bdba65d
pr-jack <prevgenij@gmail.com>
2019-12-06 00:37:13 +0200

    06.12.19

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 import './js/'
 
 // SCSS
-import './assets/scss/modules/main.scss'
+import './assets/scss/modules/page-main.scss'
+import './assets/scss/main.scss'
 
 // CSS (example)
 // import './assets/css/main.css'
